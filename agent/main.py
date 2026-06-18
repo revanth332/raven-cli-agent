04074ccd674c95daf6a18a4bf52784655eeb3c69
revathipathilanka347 <revathipathilanka347@gmail.com>
2026-06-18 19:41:20 +0530

    feat: Implement 'run' command for executing shell instructions

--- agent/main.py
+++ agent/main.py
@@ -8,6 +8,7 @@ from agent.llm import get_response,get_chat_session
 
 import sys
 import subprocess
+import json
 
 app = typer.Typer()
 console = Console()
@@ -29,6 +30,10 @@ def display_welcome():
     console.print("\n")                                                                                                                              
     console.print(welcome_panel)                                                                                                                     
     console.print("[dim italic]System ready. How can I help you today?[/dim italic]\n")   
+
+@app.command()
+def test():
+    print("testng..")
 
 @app.command()
 def ask(query:str):
@@ -66,25 +71,40 @@ def debug():
 
 @app.command()
 def commit():
-    try:
-        result = subprocess.run(["git","diff","--staged"],capture_output=True,text=True)
-        staged_changes = result.stdout.strip()
+    result = subprocess.run(["git","diff","--staged"],capture_output=True,text=True)
+    staged_changes = result.stdout.strip()
+    if not staged_changes:
+        console.print("[yellow]No staged changes found. Please run 'git add' first.[/yellow]")
+        raise typer.Exit(1)
+    
+    commit_genration_prompt = f"Write a concise, professional, conventional, short git commit message based on the following diff. Only return the commit message, no markdown formatting or extra text. \n\n{staged_changes}"
+    with console.status("generating commit message..."):
+        commit_message = get_response(commit_genration_prompt)
+    console.print(f"[green]{commit_message}[/green]")
 
-        if not result:
-            console.print("[yellow]No staged changes found. Please run 'git add' first.[/yellow]")
-            raise typer.Exit(1)
+    is_user_confirmed = typer.confirm("Commit with this message?")
+    if is_user_confirmed:
+        subprocess.run(["git","commit","-m",commit_message])
+        console.print("[green]✔ commited the code successfully[green]")
+    else:
+        console.print("[yellow]commit aborted[/yellow]")
 
-        commit_genration_prompt = f"Write a concise, professional, conventional, short git commit message based on the following diff. Only return the commit message, no markdown formatting or extra text. \n\n{staged_changes}"
-        with console.status("generating commit message..."):
-            commit_message = get_response(commit_genration_prompt)
-        console.print(commit_message)
-        is_user_confirmed = typer.confirm("Commit with this message?")
-
-        if is_user_confirmed:
-            result = subprocess.run(["git","commit","-m",commit_message])
-            console.print(result)
-    except:
-        console.print("[red]Something went wrong. Please try again[/red]")
+@app.command()
+def run(instruction:str):
+    command_generation_prompt = f"Translate this natural language instruction into a Windows command line command (CMD/PowerShell). Return the response only in given JSON format. FORMAT: {{command:<command> description:<short, precise desciption about the command>}}. MUST resurn pure json. No backticks, no explanation, only pure json. If no command found then return plain text of explanation with details of similar commands. Instruction: {instruction}"
+    with console.status("generating command..."):
+        command_info = get_response(command_generation_prompt)
+        command_info_json = json.dumps(command_info)
+    if isinstance(command_info_json,dict):
+        command = command_info_json['command']
+        console.print(f"[bold cyan]{command}[/bold cyan]")
+        should_execute_command = typer.confirm("Execute this command?")
+        if should_execute_command:
+            subprocess.run(command,shell=True)
+        else:
+            console.print("[yellow]Command execution termiated[/yellow]")
+    else:
+        console.print(f"[yellow]{command_info}[/yellow]")
 
 if __name__ == "__main__":
     app()
