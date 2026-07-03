0d0852206de76235e8768438476153f458188ef8
revanth332 <revathipathilanka347@gmail.com>
2026-07-03 23:17:07 +0530

    chore: remove redundant test.py file

--- test.py
+++
@@ -1,9 +0,0 @@
-
-def is_palindrome(s):
-    return str(s) == str(s)[::-1]
-
-
-
-if __name__ == "__main__":
-    print(f"Is 'madam' a palindrome? {is_palindrome('madam')}")
-    print(f"Is 121 a palindrome? {is_palindrome(121)}")
